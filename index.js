472df245cffa5115898dbb1001d461b7ddc2fb8b
Noel Cabús <humblewolfstudio@gmail.com>
2022-09-07 09:50:42 +0200

    a

--- index.js
+++ index.js
@@ -3,26 +3,19 @@ require('dotenv').config();
 var Twitter = require('twitter');
 
 var client = new Twitter({
-	consumer_key: process.env.CONSUMER_KEY,
-	consumer_secret: process.env.CONSUMER_SECRET,
-	access_token_key: process.env.ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 
 axios.get("https://api.preciodelaluz.org/v1/prices/all?zone=PCB").then(response => {
     var franges = response.data;
     var bestFranges = getBestFranges(franges);
-    getCheapestFranja(franges);
-    getExpensiestFranja(franges);
-    returnBestFranja(bestFranges, franges);
-    client.post('statuses/update', {status: 'I am a tweet'}, function(error, tweet, response) {
-        if (!error) {
-            
-        } else{
-            console.log(error);
-        }
-      });
+    var cheapestFranja = getCheapestFranja(franges);
+    var expensiestFranja = getExpensiestFranja(franges);
+    var bestFranja = returnBestFranja(bestFranges, franges);
 }).catch(error => {
     console.log(error);
 });
@@ -52,7 +45,7 @@ function getCheapestFranja(franges) {
     }
     cheapestFranja = cheapestFranja.split("-");
     var cheapestFranjaMaco = cheapestFranja[0] + "h - " + cheapestFranja[1] + "h";
-    console.log("Mejor franja de precio: " + cheapestFranjaMaco + " a " + (cheapestPrice / 1000).toFixed(4) + "€/kWh");
+    return "Mejor franja de precio: " + cheapestFranjaMaco + " a " + (cheapestPrice / 1000).toFixed(4) + "€/kWh";
 }
 
 function getExpensiestFranja(franges) {
@@ -66,15 +59,15 @@ function getExpensiestFranja(franges) {
     }
     expensiestFranja = expensiestFranja.split("-");
     var expensiestFranjaMaco = expensiestFranja[0] + "h - " + expensiestFranja[1] + "h";
-    console.log("Peor franja de precio: " + expensiestFranjaMaco + " a " + (expensiestPrice / 1000).toFixed(4) + "€/kWh");
+    return "Peor franja de precio: " + expensiestFranjaMaco + " a " + (expensiestPrice / 1000).toFixed(4) + "€/kWh";
 }
 
 function returnBestFranja(bestFranges, franges) {
     var bestFranja = "";
     var prices = [];
-    for(fra in bestFranges){
-        for(franja in franges){
-            if(bestFranges[fra] == franja){
+    for (fra in bestFranges) {
+        for (franja in franges) {
+            if (bestFranges[fra] == franja) {
                 prices.push(franges[franja].price);
             }
         }
@@ -83,7 +76,7 @@ function returnBestFranja(bestFranges, franges) {
     var final = bestFranges[bestFranges.length - 1].split("-")
     bestFranja = inicio[0] + "h - " + final[1] + "h"
     var precioMedio = calcularPrecioMedio(prices);
-    console.log("Horas del dia con mejor precio: " + bestFranja + " con una media de " + (precioMedio / 1000).toFixed(4) + "€/kWh");
+    return "Horas del dia con mejor precio: " + bestFranja + " con una media de " + (precioMedio / 1000).toFixed(4) + "€/kWh";
 }
 
 function calcularPrecioMedio(precios) {
@@ -92,6 +85,16 @@ function calcularPrecioMedio(precios) {
         resultado += precios[precio];
     }
     return (resultado / precios.length);
+}
+
+function postTweet(tweet) {
+    client.post('statuses/update', { status: tweet }, function (error, tweet, response) {
+        if (!error) {
+
+        } else {
+            console.log(error);
+        }
+    });
 }
 
 //(TODO) hacer que suba un tweet cuando vaya a empezar la franja alta y otro cuando la franja baja
